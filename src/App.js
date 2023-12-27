a1d60f18705d0a8909d3fc254de7ca853836dff3
Wiktoria Wolczyk <wwolczyk@icloud.coml>
2023-12-27 22:37:37 +0100

    "done List of products"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { MyList } from "./products-list/products-list1";
+import { useState } from "react";
 
 function App() {
+  const [searchText, setSearchText] = useState("");
+
+  const searchProducts = (event) => {
+    setSearchText(event.target.value);
+  };
+
   return (
-    <div className="App">
+    <>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span className="search">Search:</span>
+
+        <input onChange={searchProducts} className="search-panel"></input>
       </header>
-    </div>
+      <main className="my-list">
+        <p>Search text: {searchText}</p>
+        <MyList dupa={searchText} />
+      </main>
+    </>
   );
 }
 
